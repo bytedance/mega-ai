aa149dff8f4c0bd708e174a16595277e5be28065
zhangliang <zhangliang.thanks@bytedance.com>
2020-06-08 14:29:59 +0800

    update mag_xgbutil definiton

--- mag_util/mag_xgbutil.py
+++ mag_util/mag_xgbutil.py
@@ -1,13 +1,18 @@
 import pandas as pd
 
-def get_xgboost_feature_imps(xgb_model_, ascending=False):
+def get_xgboost_feature_imps(xgb_model_, importance_type='total_gain', is_sorted=False, filter_zero_imps=False):
     """Get xgboost feature importance from XGBoost Model
+    params:
     * xgb_model_: XGBoost model instance
-    * ascending: False/True, sort feature_df_sorted_['importances'] by 'ascending'.
+    * importance_type: one of ["gain", "weight", "cover", "total_gain" or "total_cover"] 
+    * is_sorted: False/True, sort feature_df_sorted_['importance'] by 'desc'.
+    * filter_zero_imps: False/True, weather keep feature which importance = 0.0 feature_imps_df_
+    result:
+    * feature_imps_df_: pd.DataFrame() with column ['feature', 'importance']
     Note:
     * This method get feature importance as type of importance_type
     """
-    feature_df_ = pd.DataFrame()
-    feature_df_sorted_ = feature_df_
 
-    return feature_df_, feature_df_sorted_
+    feature_imps_df_ = pd.DataFrame()
+
+    return feature_imps_df_
