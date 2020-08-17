edef76a3ac49be0e2bef6220e14d3a344d154a13
dancerNing <huangning.honey@bytedance.com>
2020-08-17 16:53:41 +0800

    modify version to 1.0.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ except IOError:
 
 setup(
       name='byted-'+NAME,
-      version='2.0.0',
+      version='1.0.1',
       description=DESCRIPTION,
       long_description=long_description,
       long_description_content_type="text/markdown",
