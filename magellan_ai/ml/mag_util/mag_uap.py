b1d9272d7a01a63f096908bccb89f800c9e92248
刘京升 <rushdcat@gmail.com>
2020-09-10 12:02:58 +0800

    translate

--- magellan_ai/ml/mag_util/mag_uap.py
+++ magellan_ai/ml/mag_util/mag_uap.py
@@ -33,37 +33,37 @@ def feature_coverage_in_diff_people(
         col_handler_dict={},
         cols_skip=[],
         is_sorted=True):
-    """对于两人群根据样本特征列计算特征覆盖率
+    """calculate feature coverage for 2 groups
     Parameters
     -----------
     df_ : DataFrame
-        输入文件
+        Input file.
 
     group_col : list, ndarray, Series or DataFrame
-        表示人群的标签所在的列
+        The column about group.
 
     group_dict : dict, optional
-        group 标签的别名，不出现在此 dict 的标签，别名等于自身
+        Alias of group's name，default is equal to itself.
 
     col_no_cover_dict : dict, optional
-        自定义特征指定数据类型的非覆盖值
+        A custom feature specifies a non overriding value for the data type.
         default = {'int64': [-1], 'float64': [-1.0],
                     str': ["-1", "unknown"],
                     'object': ["-1", "unknown"], 'bool': []}
 
     col_handler_dict : dict, optional
-        指定特征数据类型的覆盖率计算方法.
+        Dictionary of no-covered features.
 
     cols_skip : List, optional
-        忽略计算特征覆盖率的特征名称.
+        Ignore feature names for which feature coverage is calculated.
 
     is_sorted : bool, optional
-        是否对特征覆盖率进行排序.
+        Whether to sort feature coverage
 
     Returns
     ----------
     feat_coverage_df : DataFrame
-        特征的覆盖率以及相应的数据类型
+        The coverage of features and the corresponding data types
 
     Example
     -----------
@@ -83,14 +83,14 @@ def feature_coverage_in_diff_people(
 
     Notes
     -------
-    group_df_col 必有两种有效标签
+    There must be two valid tags in column group_df_col
     """
-    # 默认非覆盖标签
+    # Default non overlay label
     if not col_no_cover_dict:
         col_no_cover_dict = {'int64': [-1], 'float64': [-1.0],
                              'str': ["-1", "unknown"],
                              'object': ["-1", "unknown"], 'bool': []}
-    # 取出两人群个特征信息
+    # Take out the characteristic information of the two groups
     groups = df_.groupby(group_col)
     group_dfs, indexs = [], []
     for index, group_df in groups:
@@ -98,13 +98,13 @@ def feature_coverage_in_diff_people(
             continue
         indexs.append(index)
         group_dfs.append(group_df)
-    # 人群种类数不为 2，抛出异常
+    # If the number of population types is not 2, throw an exception
     try:
         if len(group_dfs) != 2:
             raise Exception("人群种类数不为 2")
     except Exception as err:
         print(err)
-    # 调用 metrics 库，生成两人群标签覆盖率 DataFrame
+    # Generate datafraame
     df1 = mag_metrics.cal_feature_coverage(
         group_dfs[0],
         col_no_cover_dict,
@@ -118,11 +118,11 @@ def feature_coverage_in_diff_people(
         cols_skip,
         is_sorted)
 
-    # 修改两个 coverage 列信息
+    # Modify two coverage column information
     df1.columns = ['feature', 'coverage_%s' % indexs[0], 'feat_type']
     df2.columns = ['feature', 'coverage_%s' % indexs[1], 'feat_type']
     del df1['feat_type']
-    # 合并 DataFrame
+    # Merge DataFrame
     res_df = pd.merge(df1, df2, how="inner", on="feature")
 
     return res_df
@@ -136,40 +136,44 @@ def single_enum_feat_eval_diff_people(
         col_no_cover_dict={},
         draw_pics=False):
     """
-    1. 分析两人群在单个枚举类特征上差异。特征的例子：性别，职业，最喜欢的 app
-    2. 特征取值为 2 种时，进行卡方检验，特征大于 2 种时，计算 psi 值。其它统计方法后续可以添加。
+    1. The differences between the two groups in the characteristics of single
+        enumeration class were analyzed.
+        Examples of features: gender, occupation, favorite app
+    2. Chi square test was performed when the characteristic values were 2,
+        the PSI value was calculated when the characteristics were more than 2.
+        Other statistical methods can be added later.
 
     Parameters:
     ------------
     group_df_col : Series
-        人群包所在的列
+        The column about group.
 
     feature_df_col : Series
-        需要分析的特征所在的列
+        The column in which the feature to be analyzed is located
 
     group_dict : dict, optional
-        group 标签的别名，不出现在此 dict 的标签，别名等于自身
+        Alias of group's name，default is equal to itself.
 
     feature_dict : dict, optional
-        feature 标签别名，不出现在此 dict 的标签，别名等于自身
+        Alias of feature's name，default is equal to itself.
 
     col_no_cover_dict : dict, optional
-        非覆盖特征字典
+        dictionary of no-covered features.
 
     draw_pics : bool, optional
-        是否需要绘制图片，默认为 False
+        Whether need to draw pictures, default is equal to false.
 
     Returns:
     -----------
     report : dict
         "DataFrame" : DataFrame
-            人群各特征比例
+            Proportion of population features.
         "chi2" :float64
-            特征数为2时，计算卡方值
+            when the number of features is equal to 2, calculate chi2-square.
         "psi" : float64
-            特征数大于2，计算psi
+           If the feature number is greater than 2, calculate psi.
         "result" : str
-            差异性的结论
+            Conclusion of difference.
 
     Examples
     ------------
@@ -203,32 +207,40 @@ def single_enum_feat_eval_diff_people(
 
     Notes:
     ----------
-    1. group_df_col 必有两种有效标签，featute_df_col 必有至少两种有效标签，否则抛出异常。
-    2. 目前支持 featute_df_col = 2 进行卡方检验，featute_df_col > 2 通过 psi 分析差异。
-    3. featute_df_col >= 25 时，不会绘制饼图。
-    4. 卡方检验中 H0：π1=π2，H1：π1≠π2，
-    5. 卡方检验自由度为 1，卡方值 >= 3.841，p-value <= 0.05 可以认为有显著差异。
-    6. psi 相关信息：
-        < 0.1 :  差异很小
-        0.1 ~ 0.25 : 差异一般
-        >= 0.25 : 差异很大
+    1. group_df_col must have two valid tags, featute_ Df_ Col must have
+        at least two valid tags,
+        otherwise an exception is thrown.
+    2. If the number of feature type is equal to 2, calculate chi square,
+        else if the number of feature type is greater than 2, psi will
+        be calculated
+    3. If the number of feature type is greater than 25, the pie chart
+        will not be drawn.
+    4. In Chi square test, H0: π1 = π2, H1: π1 ≠ π2,
+    5. Chi square test degree of freedom is 1, chi square value > = 3.841,
+    p-value < = 0.05, which can be considered as significant difference.
+    6. Psi related information:
+        < 0.1: the difference is small
+        0.1 ~ 0.25: average difference
+        >= 0.25: very different
     """
-    # 默认非覆盖标签
+    # default no_cover features
     if not col_no_cover_dict:
         col_no_cover_dict = {'int64': [-1], 'float64': [-1.0],
                              'str': ["-1", "unknown"],
                              'object': ["-1", "unknown"], 'bool': []}
 
-    # 合并 group_df_col, featutre_df_col, 删掉非覆盖标签所在的行，结果保存在 df 中
+    # merge group_df_col, featutre_df_col, delete rows
+    # where no-cover feature locate at
     group_list, feature_list = [], []
     for group, feature in zip(group_df_col, feature_df_col):
-        # 保留 group 和 feature 同时有效的信息
+        # Keep the information that the group and feature are valid
+        # at the same time
         if group not in col_no_cover_dict[str(
                 group_df_col.dtype)] and feature not in col_no_cover_dict[
                     str(feature_df_col.dtype)]:
             group_list.append(group)
             feature_list.append(feature)
-            # 把不在的 dict 的标签别名设为自身
+            # Set the tag alias of the absent dict to itself
             if group not in group_dict:
                 group_dict[group] = group
             if feature not in feature_dict:
@@ -236,11 +248,13 @@ def single_enum_feat_eval_diff_people(
 
     df = pd.DataFrame({'group': group_list, 'feature': feature_list})
 
-    # 人群种类，特征种类
+    # Population type, feature type
     group_counter = len(df.groupby("group"))
     feature_counter = len(df.groupby('feature'))
 
-    # 异常处理，特征种类数少于 2，人群种类数不等于 2 时，抛出异常
+    # Exception handling: when the number of characteristic types is
+    # less than 2 and the number of population types is not equal to 2,
+    #  an exception is thrown
     try:
         if feature_counter <= 1:
             raise Exception("此标签特征种类小于 2，无法差异分析")
@@ -252,13 +266,15 @@ def single_enum_feat_eval_diff_people(
     except Exception as err:
         print(err)
 
-    # 计算各人群中各 feature 个数与比例
-    counter, ratio = {}, {}  # 人群标签向其特征信息的映射
+    # The number and proportion of features in each population were calculated
+    counter, ratio = {}, {}
+    # Mapping of group tags to their feature information
     for group_tag in group_dict:
-        tmp_counter, tmp_ratio = {}, {}  # group_tag 人群中，特征标签向其出现次数比例的映射
-        # 计算 group_tag 人群中 feature_tag 出现次数
+        tmp_counter, tmp_ratio = {}, {}
+        # The mapping of feature tags to their frequency ratio in group
+        # calculate frequency of occurrence about feature_tag in group_tag
         total = df.groupby("group").get_group(
-            group_tag).shape[0]  # group_tag 人数
+            group_tag).shape[0]
         for feature_tag in feature_dict:
             cnt = df.groupby("group").get_group(group_tag).groupby(
                 "feature").get_group(feature_tag).shape[0]
@@ -268,24 +284,28 @@ def single_enum_feat_eval_diff_people(
         ratio[group_tag] = tmp_ratio
 
     if feature_counter == 2:
-        # 卡方检验
-        # 生成 dataFrame，两人群中两种标签出现次数与占比
-        group_tag0, group_tag1 = group_dict.keys()  # 各 group 标签
-        group0, group1 = group_dict.values()  # 各 group 标签别名
-        feature0, feature1 = feature_dict.values()  # 每种 feature 标签的别名
-        cnt0, cnt1 = counter[group_tag0], counter[group_tag1]  # 两个组中，每个特征:出现几次
-        ratio0, ratio1 = ratio[group_tag0], ratio[group_tag1]  # 两个组中，每个特征:组内占比
+        # chi square test
+        # generate dataFrame，Frequency and proportion of two kinds of tags
+        #  in two groups
+        group_tag0, group_tag1 = group_dict.keys()  # group tags
+        group0, group1 = group_dict.values()  # alias of group tags
+        feature0, feature1 = feature_dict.values()
+        # Alias for each feature tag
+        cnt0, cnt1 = counter[group_tag0], counter[group_tag1]
+        # In both groups, each feature: how many times does it appear
+        ratio0, ratio1 = ratio[group_tag0], ratio[group_tag1]
+        # In both groups, each characteristic: intra group share
         res_df = pd.DataFrame({'features': [feature0, feature1],
                                "%s 人数" % group0: list(cnt0.values()),
                                "%s 比例" % group0: list(ratio0.values()),
                                "%s 人数" % group1: list(cnt1.values()),
                                "%s 比例" % group1: list(ratio1.values())})
-        # 卡方计算
+        # calculate chi-square
         a, b, c, d = list(cnt0.values()) + list(cnt1.values())
         n = a + b + c + d
         chi2 = 1.0 * n * (a * d - b * c) * (a * d - b * c) / \
             (a + b) / (a + c) / (b + d) / (c + d)
-        # 绘制 barh
+        # draw barh
         if draw_pics:
             index = [group0, group1]
             df_barh1 = pd.DataFrame({feature0: [cnt0[0], cnt1[0]], feature1: [
@@ -295,32 +315,35 @@ def single_enum_feat_eval_diff_people(
                 {feature0: [ratio0[0], ratio1[0]], feature1: [
                                     ratio0[1], ratio1[1]]}, index=index)
             df_barh2.plot.barh(stacked=True)
-        # 生成报告
+        # generate report
         report = {"DataFrame": res_df, "chi2": chi2}
         report["result"] = "根据卡方检验，两人群分布无明显差异" \
             if chi2 <= 3.841 else "根据卡方检验，两人群分布有明显差异"
         return report
     else:
-        # psi 计算
+        # calculate psi
         def calpsi(a, e):
             e = max(e, 0.0005)
             a = max(a, 0.0005)
             return (a - e) * np.log(a / e)
+        # Frequency of various features in two populations
+        portion0, portion1 = ratio.values()
+        group0, group1 = group_dict.values()  # Group tag alias
 
-        portion0, portion1 = ratio.values()  # 两个人群各种特征出现频率
-        group0, group1 = group_dict.values()  # 各 group 标签别名
-
-        # 如果portion0的分组中key没有落入portion1，那么对应portion1的取值为0
+        # If the key in the grouping of portion0 does not fall into portion1,
+        # then the corresponding value of portion1 is 0
         for x in portion0:
             if x not in portion1:
                 portion1[x] = 0
 
-        # 如果portion0的分组中key没有落入portion1，那么对应portion1的取值为0
+        # If the key in the grouping of portion1 does not fall into portion0,
+        # then the corresponding value of portion0 is 0
         for x in portion1:
             if x not in portion0:
                 portion0[x] = 0
 
-        # 分别将两组人群变成数据框并根据特征值进行拼接
+        # The two groups of people were transformed into data frames and
+        #  stitched according to the eigenvalues
         group0_df = pd.DataFrame.from_dict(
             portion0,
             orient="index",
@@ -335,7 +358,7 @@ def single_enum_feat_eval_diff_people(
                 "index": "feat_value"})
         res_df = pd.merge(group0_df, group1_df, how="inner", on="feat_value")
 
-        # psi 计算
+        # calculate psi
         psi_li = []  # psi List
         psi_sum = 0
         for i in sm.range(len(res_df)):
@@ -343,12 +366,14 @@ def single_enum_feat_eval_diff_people(
             psi_sum += psi_value
             psi_li.append(psi_value)
 
-        # 生成显示标签在人群中占比的 DataFrame，并按差异大小，对特征标签排序
+        # The data frame showing the proportion of tags
+        # in the population is generated,
+        # and the feature tags are sorted according to the difference size
         res_df["psi"] = psi_li
         res_df = res_df.sort_values(by=['psi'], ascending=False)
         res_df.index = [i for i in sm.range(res_df.shape[0])]
 
-        # 绘制两人群饼图
+        # Draw a pie chart of two groups
         if draw_pics:
             limit = 25
             if len(portion0) <= limit:
@@ -364,7 +389,7 @@ def single_enum_feat_eval_diff_people(
             else:
                 print("此特征种类数太多，不适合绘制饼图")
 
-        # 生成报告
+        # generate report
         report = {"DataFrame": res_df, "psi": psi_sum}
         if psi_sum < 0.1:
             report["result"] = "差异很小"
@@ -377,9 +402,11 @@ def single_enum_feat_eval_diff_people(
 
 
 """
-1. 从均值分析两人群中，较为连续的特征差异（例如：概率类标签）
-2. 绘制两人群 hist
-3. 目前支持 t-test / Welch ，效果不佳，后续检验方法需要更新。
+1. Analyze the more continuous feature differences
+between the two groups from the mean value (e.g. probability label)
+2. Draw the hist of two groups
+3. At present, t-test / Welch is supported, the effect is not good,
+and the follow-up test method needs to be updated.
 """
 
 
@@ -393,25 +420,25 @@ def single_continuity_feat_eval_diff_people(
     Parameters:
     -----------
     group_df_col : Series
-        人群包所在的列
+        The column about group.
     feature_df_col : Series
-        需要分析的特征所在的列
+        The column in which the feature to be analyzed is located
     group_dict : dict, optional
-        group 标签的别名，不出现在此 dict 的标签，别名等于自身
+        Alias of group's name，default is equal to itself.
     col_no_cover_dict : dict, optional
-        非覆盖特征字典
+        Dictionary of no-cover features
     draw_pics : bool, optional
-        是否需要绘制图片，默认为 False
+        Whether need to draw pictures, default is equal to false.
 
     Returns:
     ------------
     report : dict
         "DataFrame" : DataFrame
-            两人群均值方差
+            Mean variance of two populations
         "p-value" : float
-            t-test / Welch 检验结果
+            t-test / Welch's result
         "result" : str
-            差异性的结论
+            Difference conclusion
 
     Example
     ----------
@@ -426,66 +453,71 @@ def single_continuity_feat_eval_diff_people(
 
     Notes:
     -------------
-    H0 两分布均值相同，H1 两分布均值不同，此函数根据两人群方差、样本数来选择 t-test / Welch 检验
+    The mean values of H0 and H1 are the same,
+    but the mean values of H1 are different.
+    This function selects t-test / Welch test according to
+    the variance of two populations and the number of samples
     """
-    # 默认非覆盖
+    # no-cover values
     if not col_no_cover_dict:
         col_no_cover_dict = {'int64': [-1], 'float64': [-1.0],
                              'str': ["-1", "unknown"],
                              'object': ["-1", "unknown"], 'bool': []}
-    # 合并 group_df_col, featutre_df_col, 删掉非覆盖标签所在的行，结果保存在 df 中
+    # merge group_df_col, featutre_df_col
     group_list, feature_list = [], []
     for group, feature in zip(group_df_col, feature_df_col):
-        # group 和 feature 同时被覆盖才保留
+        # be coverd at same time, in group and feature.
         if group not in col_no_cover_dict[str(
                 group_df_col.dtype)] and feature > -EPS:
             group_list.append(group)
             feature_list.append(feature)
-            # 将不在 group_dict 中的别名设为自身
             if group not in group_dict:
                 group_dict[group] = group
 
     group0, group1 = list(group_dict.values())
     df = pd.DataFrame({'group': group_list, 'feature': feature_list})
 
-    # 计算人群种类数
+    # group counter
     group_counter = len(df.groupby("group"))
-    # 当人群种类数不等于 2 时，抛出异常
+    # If group_counter = 2, throw exception
     try:
         if group_counter != 2:
             raise Exception("人群种类数不为 2")
     except Exception as err:
         print(err)
 
-    # 当 feature 为 obj 时，抛出异常
+    # If feature is equal to obj，throw exception
     try:
         if str(df['feature'].dtype) == "object":
             raise Exception("fearute 为 object")
     except Exception as err:
         print(err)
 
-    # 计算两个人群包均值与方差，并生成 DataFrame
+    # The mean value and variance of the two populations were calculated,
+    # and the dataframe was generated
     groups = df.groupby('group')
     vec0 = groups.get_group(0)['feature']
     vec1 = groups.get_group(1)['feature']
     features = ['mean', 'std']
-    info0 = [vec0.mean(), vec0.std()]  # 计算第一组均值、方差
-    info1 = [vec1.mean(), vec1.std()]  # 计算第二组均值、方差
+    info0 = [vec0.mean(), vec0.std()]
+    # The mean and variance of the first group were calculated
+    info1 = [vec1.mean(), vec1.std()]
+    # The mean and variance of the second group were calculated
     res_df = pd.DataFrame(
         data={'features': features, group0: info0, group1: info1})
 
-    # 绘制分布柱状图
+    # Draw distribution histogram
     if draw_pics:
         plt.hist(vec0, bins=100, color="#FF0000", alpha=.9), plt.title(
             "%s" % group0), plt.show()
         plt.hist(vec1, bins=100, color="#FF0000", alpha=.9), plt.title(
             "%s" % group1), plt.show()
 
-    # t 检验 / Welch 检验
+    # t test / Welch test
     L = stats.ttest_ind(vec0, vec1, equal_var=True if stats.levene(
         vec0, vec1).pvalue <= 0.05 else False)
 
-    # 生成结论报告
+    # generte reports
     report = {"DataFrame": res_df, "p-value": L.pvalue}
     report["result"] = "有较大差异" if L.pvalue < 0.05 else "无明显差异"
     return report
