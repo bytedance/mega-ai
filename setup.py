8ee79c103a5df2f9dcc26156f07bdc3459d5e0eb
huangning.honey <huangning.honey@bytedance.com>
2020-11-22 17:17:13 +0800

    update version 1.3.2 new

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ except IOError:
 
 setup(
       name='byted-'+NAME,
-      version='1.3.1',
+      version='1.3.2',
       description=DESCRIPTION,
       long_description=long_description,
       long_description_content_type="text/markdown",
